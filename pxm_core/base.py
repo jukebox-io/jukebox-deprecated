c1a50f29d9fce6719435b29ff7a6eff893978582
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2021-12-26 22:47:35 +0530

    Version Reading Redirected to Pubspec file in Client module

--- pxm_core/base.py
+++ pxm_core/base.py
@@ -9,20 +9,12 @@ async def _common_parameters(version: ApiVersion) -> dict:
     """Helper function to get all the common parameters for all REST endpoints."""
     return {'version': version}
 
-
-def _read_version_file() -> str:
-    """Read the version file located at root dir"""
-    pxm_home = os.getenv('pxm.home')
-    with open(f'{pxm_home}/version.txt', 'r') as ver_file:
-        return ver_file.read().strip()
-
-
 # // ---------------------------------------------------------------------------------------------------- router def
 
 # FastAPI Router
 router = FastAPI(
     title='ProjectX Music API',
-    version=_read_version_file(),
+    version=os.getenv('pxm.version') or 'n/a',
     dependencies=[Depends(_common_parameters)]
 )
 router.router.prefix = '/api/{version}'  # FastAPI do not allow mentioning prefix during router initialization
--- startServer.py
+++ startServer.py
@@ -4,19 +4,36 @@ import pathlib
 import sys
 
 import dotenv
+import yaml
+
+
+def _read_version_string() -> str:
+    try:
+        pxm_home = os.environ['pxm.home']
+        pubspec_file_path = f'{pxm_home}/pxm_client/pubspec.yaml'
+        with open(pubspec_file_path, 'r') as pubspec_file:
+            pubspec_props: dict = yaml.load(pubspec_file, Loader=yaml.FullLoader)
+        return pubspec_props['version']
+    except:
+        return ''
+
 
 if __name__ == '__main__':
     pxm_home = os.path.dirname(pathlib.Path(__file__).absolute())
-    conf_file_path_regex = f'{pxm_home}/.configs/*.conf'
-
-    # Load Configurations
-    for conf_file in glob.glob(conf_file_path_regex):
-        dotenv.load_dotenv(dotenv_path=conf_file)
+    conf_file_path_regex = f'{pxm_home}/pxm_configurations/*.conf'
 
     # Add root dir to python path and environment
     sys.path.append(pxm_home)
     os.environ['pxm.home'] = pxm_home
 
+    # Load version string
+    os.environ['pxm.version'] = _read_version_string()
+
+    # Load Configurations
+    for conf_file in glob.glob(conf_file_path_regex):
+        dotenv.load_dotenv(dotenv_path=conf_file)
+
     # Start Server
     from pxm_server.base import start_server
+
     start_server()
