53c9cbcb12eb615ea1dcb4e5dceda29cd398fffc
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2022-01-05 15:44:49 +0530

    Issue with server spin-up in staging environment solved

--- db_migrations/migration.py
+++ db_migrations/migration.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from alembic import command
 from alembic.config import Config
 from sqlalchemy.engine.url import make_url
@@ -10,8 +12,16 @@ def run_migrations(script_location: str, db_url: str):
 
     alembic_cfg.print_stdout('INFO: Running DB Migration on %r', make_url(db_url))
     try:
+        # Check and create path/to/script_location/versions
+        # Git ignores all empty directories, but it is a necessary for this path to exist
+        pathlib.Path(f'{script_location}/versions').mkdir(parents=True, exist_ok=True)
+
+        # Run alembic upgrade command
         command.upgrade(alembic_cfg, 'head')
+
+        # Display current status
         command.history(alembic_cfg, indicate_current=True)
+
         alembic_cfg.print_stdout('INFO: DB Migrations Successful')
     except Exception as e:
         alembic_cfg.print_stdout('INFO: DB Migrations Failed')
