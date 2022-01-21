38751860f3f2ef44eb08b6ae350fbd9603c06a35
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2022-01-21 10:48:17 +0530

    Rapidoc Support Added

---
+++ pxm_core/docs_builder.py
@@ -0,0 +1,54 @@
+from fastapi.responses import HTMLResponse
+
+
+def get_rapidoc_html(
+        *,
+        openapi_url: str,
+        title: str,
+        rapidoc_js_url: str = "https://cdn.jsdelivr.net/npm/rapidoc@9.1.3/dist/rapidoc-min.js",
+        rapidoc_favicon_url: str = None,
+        with_google_fonts: bool = True,
+) -> HTMLResponse:
+    html = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>{title}</title>
+        <!-- needed for adaptive design -->
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes">
+        """
+    if with_google_fonts:
+        html += """<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600&family=Roboto+Mono
+        &display=swap" rel="stylesheet"> """
+    if rapidoc_favicon_url:
+        html += """
+        <link rel="shortcut icon" href="{rapidoc_favicon_url}">
+        """
+    html += f"""
+        <style>
+            rapi-doc{{
+                width:100%;
+            }}
+        </style>
+        <script type="module" src="{rapidoc_js_url}"></script>
+        </head>
+        <body>
+        <rapi-doc
+            allow-authentication = "false" 
+            allow-search = "true"
+            show-info = "true"
+            spec-url = "{openapi_url}"
+            theme = "light"
+            show-header = "false" 
+            render-style = "read"
+            allow-try = "true"
+            regular-font = "Open Sans"
+            mono-font = "Roboto Mono"
+            schema-style = "table"
+        >
+        </rapi-doc>
+        </body>
+        </html>
+        """
+    return HTMLResponse(html)
--- pxm_core/router.py
+++ pxm_core/router.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from pxm_commons.enums import Config
+from pxm_core.docs_builder import get_rapidoc_html
 from pxm_resources.api_resource import router as api_router
 from pxm_utils.config_utils import read_config
 
@@ -8,9 +10,17 @@ from pxm_utils.config_utils import read_config
 router = FastAPI(
     title='ProjectX Music API',
     version=read_config(Config.APP.VERSION) or 'n/a',
-    docs_url='/docs',  # Will be removed in future
-    redoc_url='/api/docs'
+    docs_url=None, redoc_url=None  # Disable builtin docs
 )
 
 # Attach API Router
 router.include_router(api_router)
+
+
+# Attach api docs endpoint
+@router.get('/api/docs', include_in_schema=False)
+async def get_rapidoc() -> HTMLResponse:
+    return get_rapidoc_html(
+        openapi_url=router.openapi_url,
+        title=f'{router.title} Docs'
+    )
