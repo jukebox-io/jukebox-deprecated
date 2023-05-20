357d2ee6baa94e0b2b32f2a333e017361d71aff1
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2023-05-20 23:56:33 +0530

    Added support for schedule job execution

--- bin/startServer.py
+++ bin/startServer.py
@@ -2,34 +2,42 @@
 #  This file is part of the JukeBox Music App and is released under the "MIT License Agreement"
 #  Please see the LICENSE file that should have been included as part of this package
 
+from multiprocessing.context import SpawnProcess
+
 from uvicorn import Server, Config
 from uvicorn.supervisors import Multiprocess
 
+from jukebox.scheduler import run_scheduler
 from jukebox.utils import get_logger, logging_config
 
 logger = get_logger()
 
-config = Config(
-    app='jukebox.main:app',
-    host='localhost',
-    port=8000,
-    workers=5,
-    log_config=logging_config,
-)
+run_config: dict = {
+    'app': 'jukebox.main:app',
+    'host': 'localhost',
+    'port': 8000,
+    'workers': 4,
+    'log_config': logging_config,
+}
 
 
-class Application(Server):
-    pass
+class Application(Multiprocess):
+
+    def __init__(self, **kwargs):
+        config = Config(**kwargs)
+        super().__init__(config, target=Server(config).run, sockets=[config.bind_socket()])
+
+    def startup(self) -> None:
+        super().startup()
+
+        # Start scheduler process
+        scheduler = SpawnProcess(target=run_scheduler)
+        scheduler.start()
+        self.processes.append(scheduler)
 
 
 if __name__ == "__main__":
-    logger.info("Starting development server at http://%s:%d/", config.host, config.port)
+    logger.info("Starting development server at http://%s:%d/", run_config['host'], run_config['port'])
     logger.info("Quit the server with CONTROL-C.")
 
-    # Configure
-    application = Application(config)
-    socket = config.bind_socket()
-    supervisor = Multiprocess(config, target=application.run, sockets=[socket])
-
-    # Run Server
-    supervisor.run()
+    Application(**run_config).run()
---
+++ jukebox/scheduler.py
@@ -0,0 +1,34 @@
+#  Copyright (c) 2023 JukeBox Developers - All Rights Reserved
+#  This file is part of the JukeBox Music App and is released under the "MIT License Agreement"
+#  Please see the LICENSE file that should have been included as part of this package
+
+import os
+
+import psutil
+from rocketry import Rocketry
+
+from jukebox.utils import get_logger
+
+logger = get_logger('scheduler')
+
+scheduler = Rocketry()
+
+
+@scheduler.task('every 30 seconds')
+async def example_task():
+    if psutil.virtual_memory().percent > 90:
+        logger.warning("Low memory alert !!!")
+
+
+def run_scheduler() -> None:
+    logger.debug("Booting scheduler with pid: %d", os.getpid())
+    try:
+        scheduler.run()
+    except KeyboardInterrupt:
+        # Don't raise the keyboard interrupt, and exit gracefully
+        pass
+    logger.debug("Stopping scheduler with pid: %d", os.getpid())
+
+
+if __name__ == '__main__':
+    run_scheduler()
