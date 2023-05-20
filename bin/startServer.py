52b26632566b2f0d5b6472ca5c750cf3cc8aa61b
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2023-05-20 20:35:42 +0530

    Modified startServer.py

--- bin/startServer.py
+++ bin/startServer.py
@@ -2,21 +2,34 @@
 #  This file is part of the JukeBox Music App and is released under the "MIT License Agreement"
 #  Please see the LICENSE file that should have been included as part of this package
 
-import uvicorn
+from uvicorn import Server, Config
+from uvicorn.supervisors import Multiprocess
 
 from jukebox.utils import get_logger, logging_config
 
 logger = get_logger()
 
-run_config: dict = {
-    'app': 'jukebox.main:app',
-    'host': 'localhost',
-    'port': 8000,
-    'workers': 5,
-    'log_config': logging_config,
-}
+config = Config(
+    app='jukebox.main:app',
+    host='localhost',
+    port=8000,
+    workers=5,
+    log_config=logging_config,
+)
+
+
+class Application(Server):
+    pass
+
 
 if __name__ == "__main__":
-    logger.info("Starting development server at http://%s:%d/", run_config['host'], run_config['port'])
+    logger.info("Starting development server at http://%s:%d/", config.host, config.port)
     logger.info("Quit the server with CONTROL-C.")
-    uvicorn.run(**run_config)
+
+    # Configure
+    application = Application(config)
+    socket = config.bind_socket()
+    supervisor = Multiprocess(config, target=application.run, sockets=[socket])
+
+    # Run Server
+    supervisor.run()
